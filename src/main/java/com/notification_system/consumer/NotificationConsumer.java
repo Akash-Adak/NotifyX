10787030eefa57f10cb06b07bf13230b369a8b67
Akash Adak <akashadak00023@gmail.com>
2026-03-31 21:04:21 +0530

    add websocket config and chnaged the notification consumers

--- src/main/java/com/notification_system/consumer/NotificationConsumer.java
+++ src/main/java/com/notification_system/consumer/NotificationConsumer.java
@@ -1,13 +1,26 @@
 package com.notification_system.consumer;
 
+
+
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationConsumer {
 
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public NotificationConsumer(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
     @KafkaListener(topics = "processed-notifications", groupId = "notification-group")
     public void consume(String message) {
-        System.out.println("FINAL NOTIFICATION 👉 " + message);
+
+        System.out.println("FINAL 👉 " + message);
+
+        // 🚀 SEND TO FRONTEND
+        messagingTemplate.convertAndSend("/topic/notifications", message);
     }
 }
